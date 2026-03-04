19120044c3e937945a66ff74975b660d97172537
Elsa Tsia <ejj_t@outlook.com>
2026-03-05 10:25:44 +1100

    adding unversioned files

---
+++ src/main/java/com/scb/OptionsDashboard/OptionsDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.scb.OptionsDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptionsDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptionsDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scb/OptionsDashboard/OptionsDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scb.OptionsDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptionsDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
